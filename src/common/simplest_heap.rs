c06b57bb584b8e12de2c8623277096218f10abca
Jalon Wong <jalonwong@gmail.com>
2025-12-24 10:26:38 -0600

    Simplify heap

--- src/common/simplest_heap.rs
+++ src/common/simplest_heap.rs
@@ -1,9 +1,9 @@
-use crate::common::critical_section::Mutex;
 use core::{
     alloc::{GlobalAlloc, Layout},
     cell::UnsafeCell,
     mem::MaybeUninit,
     ptr,
+    sync::atomic::{AtomicUsize, Ordering},
 };
 
 /// The simplest possible heap.
@@ -15,61 +15,69 @@ use core::{
 ///
 /// It is recommended that you use [embedded-alloc](https://crates.io/crates/embedded-alloc)
 pub struct Heap<const SIZE: usize> {
-    heap: Mutex<UnsafeCell<SimplestHeap<SIZE>>>,
+    arena: UnsafeCell<[MaybeUninit<u8>; SIZE]>,
+    remained: AtomicUsize,
 }
+
+unsafe impl<const SIZE: usize> Sync for Heap<SIZE> {}
 
 impl<const SIZE: usize> Heap<SIZE> {
     /// Create a new heap allocator
     pub const fn new() -> Self {
         Self {
-            heap: Mutex::new(UnsafeCell::new(SimplestHeap::new())),
+            arena: UnsafeCell::new([MaybeUninit::uninit(); SIZE]),
+            remained: AtomicUsize::new(SIZE),
         }
     }
 
     /// Returns an estimate of the amount of bytes in use.
-    pub fn used(&self) -> usize {
-        critical_section::with(|cs| unsafe { &*self.heap.borrow(cs).get() }.remained())
+    pub fn remained(&self) -> usize {
+        self.remained.load(Ordering::Relaxed)
     }
 }
 
 unsafe impl<const SIZE: usize> GlobalAlloc for Heap<SIZE> {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        critical_section::with(|cs| unsafe { &mut *self.heap.borrow(cs).get() }.alloc(layout))
+        // `Layout` contract forbids making a `Layout` with align=0, or align not power of 2.
+        // So we can safely use a mask to ensure alignment without worrying about UB.
+        let align_mask_to_round_down = !(layout.align() - 1);
+
+        let mut old_remained = self.remained.load(Ordering::Relaxed);
+        loop {
+            if layout.size() > old_remained {
+                return ptr::null_mut();
+            }
+
+            let remained = (old_remained - layout.size()) & align_mask_to_round_down;
+            match self.remained.compare_exchange_weak(
+                old_remained,
+                remained,
+                Ordering::SeqCst,
+                Ordering::Relaxed,
+            ) {
+                Err(x) => old_remained = x,
+                Ok(_) => {
+                    return unsafe { (&mut *self.arena.get()).as_mut_ptr() as *mut u8 }
+                        .wrapping_add(remained);
+                }
+            }
+        }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
 }
 
-struct SimplestHeap<const SIZE: usize> {
-    arena: [MaybeUninit<u8>; SIZE],
-    remained: usize,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static HEAP: Heap<100> = Heap::new();
 
-unsafe impl<const SIZE: usize> Send for SimplestHeap<SIZE> {}
-
-impl<const SIZE: usize> SimplestHeap<SIZE> {
-    const fn new() -> Self {
-        Self {
-            arena: [MaybeUninit::uninit(); SIZE],
-            remained: SIZE,
-        }
-    }
-
-    fn remained(&self) -> usize {
-        self.remained
-    }
-
-    fn alloc(&mut self, layout: Layout) -> *mut u8 {
-        if layout.size() > self.remained {
-            return ptr::null_mut();
-        }
-
-        // `Layout` contract forbids making a `Layout` with align=0, or align not power of 2.
-        // So we can safely use a mask to ensure alignment without worrying about UB.
-        let align_mask_to_round_down = !(layout.align() - 1);
-
-        self.remained -= layout.size();
-        self.remained &= align_mask_to_round_down;
-        (self.arena.as_mut_ptr() as *mut u8).wrapping_add(self.remained)
+    #[test]
+    fn test_heap() {
+        assert_eq!(HEAP.remained.load(Ordering::Relaxed), 100);
+        let p1 = unsafe { &mut *HEAP.arena.get() }.as_mut_ptr() as *mut u8;
+        let p2 = unsafe { HEAP.alloc(Layout::new::<u32>()) };
+        assert_eq!(HEAP.remained.load(Ordering::Relaxed), 96);
+        assert_eq!(unsafe { p2.offset_from(p1) }, 96);
     }
 }
